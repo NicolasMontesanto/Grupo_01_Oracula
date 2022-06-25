aadd0dce70934b71e0029357f18ad2bba170685e
Your Name <you@example.com>
2022-06-25 12:07:45 -0300

    feat: links en categorias

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,10 +1,19 @@
 const path = require('path');
 const homePath = path.join(__dirname, '../views/home.html');
+const products = require('../data/products.json')
 
 const mainController = {
-    //home.html
+    //home
     home: (req, res) => {
         res.render('home', {titulo: "Bienvenidxs a Orácula"});
+    },
+    //category
+    category: (req, res) =>{
+        let categoria = req.params.categoria; 
+        productosAMostrar = products.filter(item => item.categoria == categoria);
+
+        res.render('category', {productosAMostrar});
     }
 };
+
 module.exports = mainController;
--- src/routers/main.routes.js
+++ src/routers/main.routes.js
@@ -1,8 +1,10 @@
+const { Router } = require('express');
 const express = require('express');
 const routes = express.Router();
 const mainController = require('../controllers/mainController');
 
 //ruta de main
 routes.get("/", mainController.home);
-
+//ruta de categoria 
+Router.get("/category/:categoria", mainController.category)
 module.exports = routes;
