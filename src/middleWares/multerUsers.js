3f27a5f2da5ffc63a6ccc1984dafd902c84f4582
juancrcabrera <juancrcabrera@gmail.com>
2022-07-05 10:36:09 -0300

    implemented: users multer

---
+++ src/middleWares/multerUsers.js
@@ -0,0 +1,17 @@
+let path = require('path');
+let multer = require('multer');
+
+const storage = multer.diskStorage({
+
+    destination:  (req, file, cb) => {
+      cb(null, './public/img/Profile-pictures')
+    },
+    filename:  (req, file, cb) => {
+      const fileName = `${Date.now()}-prodImg${path.extname(file.originalname)}`
+      cb(null, fileName);
+    }
+  })
+  
+  const upload = multer({ storage })
+
+  module.exports = upload;
